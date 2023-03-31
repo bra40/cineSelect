815d51c13feffdafd9fc589b2f896646d63bb3ce
bra40 <bruno.arnabar@yahoo.com>
2023-03-31 16:28:24 -0600

    New Film Dict

--- flask/server.py
+++ flask/server.py
@@ -7,25 +7,28 @@ CORS(app)
 @app.route("/film_dict")
 def film_dict():
     films = [
-    {'Film_title': 'We Are the Nobles',
-    'Release_year': 2013,
-    'Director': 'Gary Alazraki',
-    'Average_rating': 3.45,
-    'Letterboxd URL': 'https://letterboxd.com//film/we-are-the-nobles/',
-    'Poster_url': 'https://image.tmdb.org/t/p/w500/wsj9TvkL6Uh9leY08nuyE4OhOXW.jpg'},
-    {'Film_title': 'The Children',
-    'Release_year': 1985,
-    'Director': 'Marguerite Duras',
-    'Average_rating': 3.49,
-    'Letterboxd URL': 'https://letterboxd.com//film/the-children-1985/',
-    'Poster_url': 'https://image.tmdb.org/t/p/w500/e49Sr3Lxfk2psYhv1SzQjs7MeGo.jpg'},
-    {'Film_title': 'To Be and to Have',
-    'Release_year': 2002,
-    'Director': 'Nicolas Philibert',
-    'Average_rating': 3.92,
-    'Letterboxd URL': 'https://letterboxd.com//film/to-be-and-to-have/',
-    'Poster_url': 'https://image.tmdb.org/t/p/w500/mqB5QPNCmbF8DgsZqmyGQAGktUE.jpg'}
-    ]
+        {'Film_title': 'We Are the Nobles',
+  'Release_year': 2013,
+  'Director': 'Gary Alazraki',
+  'Average_rating': 3.46,
+  'Letterboxd URL': 'https://letterboxd.com//film/we-are-the-nobles/',
+  'Poster_url': 'https://image.tmdb.org/t/p/w500/wsj9TvkL6Uh9leY08nuyE4OhOXW.jpg',
+  'Synopsis': 'Tells the "riches to rags" story of the Nobles, three upper-class twenty-somethings that appear to have no limits to their checkbooks, and no direction in their lives. Until one day, their father tries to teach them a lesson by staging a financial scandal that forces the whole family to escape to an old house in the poor side of town, and leads the "kids" to do what they haven\'t done before: get jobs.'},
+ {'Film_title': 'RRR',
+  'Release_year': 2022,
+  'Director': 'S. S. Rajamouli',
+  'Average_rating': 4.17,
+  'Letterboxd URL': 'https://letterboxd.com//film/rrr/',
+  'Poster_url': 'https://image.tmdb.org/t/p/w500/nEufeZlyAOLqO2brrs0yeF1lgXO.jpg',
+  'Synopsis': "A fictional history of two legendary revolutionaries' journey away from home before they began fighting for their country in the 1920s."},
+ {'Film_title': '66 Scenes from America',
+  'Release_year': 1982,
+  'Director': 'Jørgen Leth',
+  'Average_rating': 3.56,
+  'Letterboxd URL': 'https://letterboxd.com//film/66-scenes-from-america/',
+  'Poster_url': 'https://image.tmdb.org/t/p/w500/eVXGdi27vR46ecBZyvKoRT1AP7S.jpg',
+  'Synopsis': 'As a visual narrative it is reminiscent of a pile of postcards from a journey, which indeed is what the film is. It consists of a series of lengthy shots of a tableau nature, each appearing to be a more or less random cross section of American reality, but which in total invoke a highly emblematic picture of the USA.'},
+  ]
 
     return jsonify(films)
 
